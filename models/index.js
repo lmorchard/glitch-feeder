03df89845321bc73c7b8168a9d8df15d57b8347e
Glitch (hello-sqlite) <none>
2018-12-20 16:46:32 +0000

    📷🐥 Checkpoint ./models/index.js:263690/506

--- models/index.js
+++ models/index.js
@@ -7,12 +7,19 @@ const knex = require("knex")({
 
 const bookshelf = require("bookshelf")(knex);
 
+bookshelf.plugin("pagination");
 bookshelf.plugin(require("bookshelf-json-columns"));
-bookshelf.plugin(require("bookshelf-uuid"));
+bookshelf.plugin(require("bookshelf-uuid"), { type: "v1" });
 
 const Feed = bookshelf.Model.extend({
   tableName: "feeds",
   uuid: true,
+  initialize: function () {
+    bookshelf.Model.prototype.initialize.apply(this, arguments);
+  },
+  save: function () {
+    bookshelf.Model.prototype.save.apply(this, arguments);
+  },
 }, {
   jsonColumns: ["data"]
 });
@@ -20,6 +27,12 @@ const Feed = bookshelf.Model.extend({
 const FeedItem = bookshelf.Model.extend({
   tableName: "feeditems",
   uuid: true,
+  initialize: function () {
+    bookshelf.Model.prototype.initialize.apply(this, arguments);
+  },
+  save: function () {
+    bookshelf.Model.prototype.save.apply(this, arguments);
+  },
 }, {
   jsonColumns: ["data"]
 });
