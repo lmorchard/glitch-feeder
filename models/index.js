74ad34f77e3b0c9038418394ca0fbfcb9e7dec8e
Glitch (hello-sqlite) <none>
2018-12-20 16:57:30 +0000

    💫😹 Checkpoint ./knexfile.js:263690/583 ./models/index.js:263690/149 ./migrations/20181220165139_initial.js:263690/11

--- models/index.js
+++ models/index.js
@@ -1,10 +1,5 @@
-const knex = require("knex")({
-  client: "sqlite3",
-  connection: {
-    filename: "./.data/sqlite.db",
-  }
-});
-
+const knexfile = require("../knexfile");
+const knex = require("knex")(knexfile.development);
 const bookshelf = require("bookshelf")(knex);
 
 bookshelf.plugin("pagination");
