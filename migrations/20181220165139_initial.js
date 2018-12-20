6b5b26c2b3a5ae918e96b7f5e553118d0c1655b8
Glitch (hello-sqlite) <none>
2018-12-20 17:07:30 +0000

    🍬🍰 Checkpoint ./migrations/20181220165139_initial.js:263690/931 ./models/index.js:263690/365

--- migrations/20181220165139_initial.js
+++ migrations/20181220165139_initial.js
@@ -1,7 +1,24 @@
-exports.up = (knex, Promise) {
-    
-};
+exports.up = knex => knex.schema
+  .createTable("Resources", t => {
+    t.string("id").primary();
+    t.timestamps();
+    t.text(
+    t.string("url").index().unique();
+  })
+  .createTable("Feeds", t => {
+    t.string("id").primary();
+    t.timestamps();
+    t.string("resource").references("Resources.id");
+  })
+  .createTable("FeedItems", t => {
+    t.string("id").primary();
+    t.timestamps();
+    t.string("feed").references("Feeds.id");
+  })
+;
 
-exports.down = function(knex, Promise) {
-  
-};
+exports.down = knex => knex.schema
+  .dropTable("Resources")
+  .dropTable("Feeds")
+  .dropTable("FeedItems")
+;
--- models/index.js
+++ models/index.js
@@ -6,8 +6,21 @@ bookshelf.plugin("pagination");
 bookshelf.plugin(require("bookshelf-json-columns"));
 bookshelf.plugin(require("bookshelf-uuid"), { type: "v1" });
 
+const Resource = bookshelf.Model.extend({
+  tableName: "Resource",
+  uuid: true,
+  initialize: function () {
+    bookshelf.Model.prototype.initialize.apply(this, arguments);
+  },
+  save: function () {
+    bookshelf.Model.prototype.save.apply(this, arguments);
+  },
+}, {
+  jsonColumns: ["data"]
+});
+
 const Feed = bookshelf.Model.extend({
-  tableName: "feeds",
+  tableName: "Feeds",
   uuid: true,
   initialize: function () {
     bookshelf.Model.prototype.initialize.apply(this, arguments);
@@ -20,7 +33,7 @@ const Feed = bookshelf.Model.extend({
 });
 
 const FeedItem = bookshelf.Model.extend({
-  tableName: "feeditems",
+  tableName: "FeedItems",
   uuid: true,
   initialize: function () {
     bookshelf.Model.prototype.initialize.apply(this, arguments);
@@ -35,6 +48,7 @@ const FeedItem = bookshelf.Model.extend({
 module.exports = {
   knex,
   bookshelf,
+  Resource,
   Feed,
   FeedItem,
 };
