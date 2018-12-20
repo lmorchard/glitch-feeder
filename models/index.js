109ec588fbcc83e91d72237c8bcc2ed058e3d85f
Glitch (hello-sqlite) <none>
2018-12-20 16:34:30 +0000

    🍿📠 Checkpoint ./models/index.js:263690/504 ./package.json:263690/3068

--- models/index.js
+++ models/index.js
@@ -1,19 +1,32 @@
 const knex = require("knex")({
   client: "sqlite3",
   connection: {
-    filename: "./.data/sqlite.db"
+    filename: "./.data/sqlite.db",
   }
 });
 
 const bookshelf = require("bookshelf")(knex);
 
+bookshelf.plugin(require("bookshelf-json-columns"));
+bookshelf.plugin(require("bookshelf-uuid"));
+
 const Feed = bookshelf.Model.extend({
+  tableName: "feeds",
+  uuid: true,
+}, {
+  jsonColumns: ["data"]
 });
 
-const Item = bookshelf.Model.extend({
+const FeedItem = bookshelf.Model.extend({
+  tableName: "feeditems",
+  uuid: true,
+}, {
+  jsonColumns: ["data"]
 });
 
 module.exports = {
   knex,
   bookshelf,
+  Feed,
+  FeedItem,
 };
