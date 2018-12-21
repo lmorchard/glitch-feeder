3300c637c2bd454360dcdbbe76dedb1c497e326b
Glitch (hello-sqlite) <none>
2018-12-21 18:56:13 +0000

    😥💶 Checkpoint ./public/app/index.js:263690/116 ./public/app/store.js:263690/2439

--- public/app/index.js
+++ public/app/index.js
@@ -1,10 +1,11 @@
 import {render, html} from "https://unpkg.com/lit-html@0.14.0/lit-html.js";
 import {repeat} from "https://unpkg.com/lit-html@0.14.0/directives/repeat.js";
+
 import { $$, addEventListeners, mapToObject } from "./utils.js";
 import { createAppStore, actions, selectors } from "./store.js";
 import "./components/index.js";
 
-export function init(appEl) {
+export async function init(appEl) {
 
   const store = createAppStore();
 
@@ -18,7 +19,12 @@ export function init(appEl) {
   };
   store.subscribe(render);
   render();
+  
+  const apiRoot = await fetch("/api/v1");
+  console.log(await apiRoot.json());
 }
+
+const fetchJson
 
 const renderApp = (appEl, props) =>
   render(appTemplate(props), appEl);
--- public/app/store.js
+++ public/app/store.js
@@ -4,75 +4,37 @@ const { createStore, combineReducers, compose } = Redux;
 
 export const defaultState = {
   ui: {
-    selectedCard: null,
-    editedCard: null,
   },
-  cards:  {
-    "alpha": {
-      id: "alpha",
-      bgcolor: "rgba(255, 0, 0, 0.2)",
-      left: 100, top: 100, width: 100, height: 100,
-      content: "Hello"
-    },
-    "beta": {
-      id: "beta",
-      bgcolor: "rgba(0, 255, 0, 0.2)",
-      left: 250, top: 100, width: 100, height: 100,
-      content: "World"
-    },
-    "gamma": {
-      id: "gamma",
-      bgcolor: "rgba(0, 0, 255, 0.2)",
-      left: 400, top: 100, width: 100, height: 100,
-      content: "Hooray"
-    },      
+  feeds: {
+  },
+  items: {
   }
 };
 
 export const selectors = {
-  cards: state => state.cards,
-  getCard: state => id => state.cards[id],
-  selectedCardId: state => state.ui.selectedCard,
-  selectedCard: state => state.cards[state.ui.selectedCard],
-  editedCardId: state => state.ui.editedCard,
-  editedCard: state => state.cards[state.ui.editedCard],
+  feeds: state => state.feeds,
+  items: state => state.items,
 };
 
 export const actions = createActions(
   {},
-  "addCard",
-  "removeCard",
-  "updateCard",
-  "selectCard",
-  "clearSelectedCard",
-  "editCard",
-  "clearEditedCard",
+  "loadFeeds",
 );
 
 export const reducers = {
+  feeds: handleActions({
+    [actions.loadFeeds]: (state, { payload: feeds = [] }) => {
+      const newFeeds = Object.assign({}, state.feeds);
+      for (let feed of feeds) {
+        newFeeds[feed.id] = feed;
+      }
+      return newFeeds;
+    }
+  }, defaultState.feeds),
+  /*
   ui: handleActions({
-    [actions.selectCard]: (state, { payload: id }) =>
-      Object.assign({}, state, { selectedCard: id }),
-    [actions.clearSelectedCard]: (state) =>
-      Object.assign({}, state, { selectedCard: null }),
-    [actions.editCard]: (state, { payload: id }) =>
-      Object.assign({}, state, { editedCard: id }),
-    [actions.clearEditedCard]: (state) =>
-      Object.assign({}, state, { editedCard: null }),
   }, defaultState.ui),
-  cards: handleActions({
-    [actions.addCard]: (state, { payload: card }) =>
-      Object.assign({}, state, { [card.id]: card }),
-    [actions.updateCard]: (state, { payload: update }) => {
-      const newCard = Object.assign({}, state[update.id], update);
-      return Object.assign({}, state, { [update.id]: newCard })
-    },    
-    [actions.removeCard]: (state, { payload: id }) => {
-      const newState = Object.assign({}, state);
-      delete newState[id];
-      return newState;
-    },
-  }, defaultState.cards)
+  */
 };
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
