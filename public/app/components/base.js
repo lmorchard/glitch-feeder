df681f60a00d2dcb60a521f18e017aac71e024e8
Glitch (famous-ticket) <none>
2018-12-25 04:48:57 +0000

    ☄️🌸 Checkpoint ./public/app/components/base.js:263690/1701 ./public/app/components/gf-feeditem.js:263690/1228

---
+++ public/app/components/base.js
@@ -0,0 +1,77 @@
+export class BaseElement extends HTMLElement {
+  constructor() {
+    super();
+    this.state = {};
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(this.template().content.cloneNode(true));    
+    this.bindEventHandlers();
+  }
+
+  template() {
+    return document.createElement("template");
+  }
+  
+  render() {
+  }
+  
+  $(selector) {
+    return this.shadowRoot.querySelector(selector);
+  }
+  
+  $$(selector) {
+    return this.shadowRoot.querySelectorAll(selector);
+  }
+  
+  get props() {
+    const out = {};
+    const names = this.constructor.observedAttributes;
+    for (let name of names) {
+      out[name] = this.getAttribute(name);
+    }
+    return out;
+  }
+  
+  fireEvent(eventType, extra = {}) {
+    this.dispatchEvent(
+      new CustomEvent(eventType, {
+        bubbles: true,
+        detail: Object.assign({}, this.props, this.state, extra)
+      })
+    );
+  }
+  
+  setAttributes(updates) {
+    for (let name in updates) {
+      this.setAttribute(name, updates[name]);
+    }
+  }
+  
+  bindEventHandlers() {
+    // HACK: magically auto self-bind any "handle*" methods
+    Object
+      .getOwnPropertyNames(this.constructor.prototype)
+      .filter(name => name.startsWith("handle"))
+      .forEach(name => this[name] = this[name].bind(this));
+  }
+  
+  scheduleRender() {
+    if (this.state.renderScheduled) { return; }
+    this.state.renderScheduled = true;
+    requestAnimationFrame(() => {
+      this.render();
+      this.state.renderScheduled = false;
+    });
+  }
+  
+  attributeChangedCallback(name, oldValue, newValue) {
+    this.scheduleRender();
+  }
+  
+  connectedCallback() {
+    this.scheduleRender();
+  }
+  
+  adoptedCallback() {
+    this.scheduleRender();
+  }
+}
