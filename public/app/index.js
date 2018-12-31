71c0117fe1fe3759de0cc1c3ef0cf2e1ff78ad75
Glitch (famous-ticket) <none>
2018-12-31 02:33:05 +0000

    ⚾️💊 Checkpoint ./public/app/index.js:263690/540 ./public/index.css:263690/6936

--- public/app/index.js
+++ public/app/index.js
@@ -1,134 +1,157 @@
-import { h, render } from "https://unpkg.com/preact@8.4.2/dist/preact.mjs?module";
-import { 
-  createAppStore, actions, selectors 
-} from "./store.js";
 import {
-  addEventListeners, 
-  mapToObject, 
-  indexBy, 
-  fetchJson, 
+  h,
+  render,
+} from "https://unpkg.com/preact@8.4.2/dist/preact.mjs?module";
+import { createAppStore, actions, selectors } from "./store.js";
+import {
+  addEventListeners,
+  mapToObject,
+  indexBy,
+  fetchJson,
   cmp,
-  rcmp 
+  rcmp,
 } from "./utils.js";
 
 export async function init(appEl) {
   const store = createAppStore();
 
   const renderApp = () =>
-    render(h(App, {
-      state: store.getState(),
-      dispatch: store.dispatch
-    }), appEl, appEl.lastElementChild);
+    render(
+      h(App, {
+        state: store.getState(),
+        dispatch: store.dispatch,
+      }),
+      appEl,
+      appEl.lastElementChild
+    );
   store.subscribe(renderApp); // TODO: Work out how to use preact-redux
   renderApp();
 
   const apiRoot = await fetchJson("/api");
   store.dispatch(actions.setApiRoot(apiRoot));
-  
-  const [
-    apiFeeds,
-    apiFolders,
-  ] = await Promise.all([
-    fetchJson(apiRoot.hrefs.feeds + "?limit=15&itemsLimit=10&itemsNew=true"),    
-    fetchJson(apiRoot.hrefs.folders),  
+
+  const [apiFeeds, apiFolders] = await Promise.all([
+    fetchJson(apiRoot.hrefs.feeds + "?limit=15&itemsLimit=10&itemsNew=true"),
+    fetchJson(apiRoot.hrefs.folders),
   ]);
 
   store.dispatch(actions.loadFeeds(apiFeeds));
   store.dispatch(actions.loadFolders(apiFolders));
-  
+
   store.dispatch(actions.setAppLoading(false));
 }
 
 const App = ({ state, dispatch }) => {
   const props = Object.assign(
     {},
-    mapToObject(
-      Object.keys(selectors),
-      name => selectors[name](state)
-    ),
-    mapToObject(
-      Object.keys(handlers),
-      name => handlers[name]({ state, dispatch })
-    ),
-  );  
+    mapToObject(Object.keys(selectors), name => selectors[name](state)),
+    mapToObject(Object.keys(handlers), name =>
+      handlers[name]({ state, dispatch })
+    )
+  );
   return AppLayout(props);
 };
 
 const handlers = {
   handleAllFeedsClick: ({ state, dispatch }) => async () => {
     const apiRoot = selectors.apiRoot(state);
-    const feeds = await fetchJson(apiRoot.hrefs.feeds + "?limit=5&itemsLimit=10"); 
+    const feeds = await fetchJson(
+      apiRoot.hrefs.feeds + "?limit=5&itemsLimit=10"
+    );
     dispatch(actions.loadFeeds(feeds));
   },
-  handleFolderClick: ({ state, dispatch }) => folder => async (ev) => {
+  handleFolderClick: ({ state, dispatch }) => folder => async ev => {
     const feeds = await fetchJson(folder.href + "&limit=10&itemsLimit=10");
     dispatch(actions.loadFeeds(feeds));
   },
-  handleFolderFeedClick: ({ state, dispatch }) => feed => async (ev) => {
+  handleFolderFeedClick: ({ state, dispatch }) => feed => async ev => {
     const result = await fetchJson(feed.hrefs.self + "?itemsLimit=10");
-    dispatch(actions.loadFeeds([ result ]));
-  }
+    dispatch(actions.loadFeeds([result]));
+  },
+  handleMoreItemsClick: ({ state, dispatch }) => feed => async ev => {
+    const lastItem = feed.items[feed.items.length - 1];
+    const url = feed.hrefs.items + "";
+    const result = await
+  },
 };
 
-const AppLayout = props => (
-  h("main", { className: "app" },
-    h("header", { className: "topnav" },
-      h("h1", null, "Glitch Feeder"),
-    ),
+const AppLayout = props =>
+  h(
+    "main",
+    { className: "app" },
+    h("header", { className: "topnav" }, h("h1", null, "Glitch Feeder")),
     props.isAppLoading
       ? h(LoadingMessage)
-      : h("section", { className: "foldersAndItems" },
+      : h(
+          "section",
+          { className: "foldersAndItems" },
           h(FoldersList, props),
-          h(ItemsList, props),
+          h(ItemsList, props)
         )
-  )
-);
+  );
 
-const LoadingMessage = () => (
-  h("div", { className: "loading" },
-    h("p", null, "Loading...")
-  )
-);
+const LoadingMessage = () =>
+  h("div", { className: "loading" }, h("p", null, "Loading..."));
 
 const FoldersList = ({
   folders,
   handleNewFeedsClick,
   handleAllFeedsClick,
   handleFolderClick,
-  handleFolderFeedClick 
+  handleFolderFeedClick,
 }) => {
-  return (
-    h("nav", { className: "feedslist" },
-      h("ul", { className: "folders" },
-        h("li", { className: "folder" },
-          h("span", {
+  return h(
+    "nav",
+    { className: "feedslist" },
+    h(
+      "ul",
+      { className: "folders" },
+      h(
+        "li",
+        { className: "folder" },
+        h(
+          "span",
+          {
             className: "foldertitle all",
-            onClick: handleAllFeedsClick
-          }, "All feeds")
-        ),
-        Object.values(folders).map(folder =>
-          h("li", { className: "folder" },
-            h("input", {
-              id: `reveal-${folder.id}`,
-              type: "checkbox",
-              className: "revealFeeds",
-            }),
-            h("label", {
+            onClick: handleAllFeedsClick,
+          },
+          "All feeds"
+        )
+      ),
+      Object.values(folders).map(folder =>
+        h(
+          "li",
+          { className: "folder" },
+          h("input", {
+            id: `reveal-${folder.id}`,
+            type: "checkbox",
+            className: "revealFeeds",
+          }),
+          h(
+            "label",
+            {
               for: `reveal-${folder.id}`,
               className: "revealFeedsLabel",
-            }, " "),
-            h("span", {
+            },
+            " "
+          ),
+          h(
+            "span",
+            {
               id: folder.id,
               className: "foldertitle",
-              onClick: handleFolderClick(folder)
-            }, folder.id),
-            h("ul", { className: "feeds" },
-              folder.feeds.map(feed => h(
-                FeedItem, {
-                  feed,
-                  handleClick: handleFolderFeedClick(feed)
-                }
-              )),
+              onClick: handleFolderClick(folder),
+            },
+            folder.id
+          ),
+          h(
+            "ul",
+            { className: "feeds" },
+            folder.feeds.map(feed =>
+              h(FeedItem, {
+                feed,
+                handleClick: handleFolderFeedClick(feed),
+              })
             )
           )
         )
@@ -137,55 +160,78 @@ const FoldersList = ({
   );
 };
 
-const FeedItem = ({ feed, handleClick }) => (
-  h("li", { className: "feed" },
-    h("span", {
-      id: feed.id,
-      className: "feedtitle",
-      onClick: handleClick
-    }, feed.title)
-  )
-);
+const FeedItem = ({ feed, handleClick }) =>
+  h(
+    "li",
+    { className: "feed" },
+    h(
+      "span",
+      {
+        id: feed.id,
+        className: "feedtitle",
+        onClick: handleClick,
+      },
+      feed.title
+    )
+  );
 
-const ItemsList = ({ feeds = [] }) => {
-  return (
-    h("section", { className: "itemslist" },
-      h("ul", { className: "feeds" },
-        feeds
-          .filter(feed => feed.items.length > 0)
-          .map(({ title, items }) =>
-            h("li", { className: "feed" },
-              h("span", { className: "feedtitle" }, title),
-              h("ul", { className: "items" },
-                items.map(item => h(Item, item))
-              )
+const ItemsList = ({ feeds = [], handleMoreItemsClick }) => {
+  return h(
+    "section",
+    { className: "itemslist" },
+    h(
+      "ul",
+      { className: "feeds" },
+      feeds
+        .filter(feed => feed.items.length > 0)
+        .map(feed =>
+          h(
+            "li",
+            { className: "feed" },
+            h("span", { className: "feedtitle" }, feed.title),
+            h(
+              "ul",
+              { className: "items" },
+              feed.items.map(item => h(Item, item))
+            ),
+            h(
+              "button",
+              {
+                className: "moreItems",
+                onClick: handleMoreItemsClick(feed),
+              },
+              "More items"
             )
           )
-      )
+        )
     )
   );
 };
 
-const Item = ({
-  title,
-  link,
-  summary,
-  text,
-  date,
-  pubdate,
-  created_at,
-}) => (
-  h("li", { className: "feeditem" },
-    h("div", { className: "details" },
+const Item = ({ title, link, summary, text, date, pubdate, created_at }) =>
+  h(
+    "li",
+    { className: "feeditem" },
+    h(
+      "div",
+      { className: "details" },
       h("a", { className: "title", href: link }, title),
-      text && h("span", { className: "text" },
-        text.length < 320 ? text : text.substr(0, 320) + "[...]")
+      text &&
+        h(
+          "span",
+          { className: "text" },
+          text.length < 320 ? text : text.substr(0, 320) + "[...]"
+        )
     ),
-    h("div", { className: "date" },
-      h("a", { className: "datelink", href: link }, 
-        (date || pubdate || created_at).replace("T", " "))
+    h(
+      "div",
+      { className: "date" },
+      h(
+        "a",
+        { className: "datelink", href: link },
+        (date || pubdate || created_at).replace("T", " ")
+      )
     )
-  )
-);
+  );
 
 export default { init };
