673c3780afb3aea0298de2044e0183fbc70cbe97
Glitch (lmo-feeder) <none>
2019-01-11 19:35:00 +0000

    🔔🙆 Checkpoint ./models/Feed.js:263690/2200 ./lib/common.js:263690/2545 ./cli/add-feed.js:263690/1790

--- cli/add-feed.js
+++ cli/add-feed.js
@@ -4,6 +4,13 @@ const AbortController = require("abort-controller");
 const fetch = require("node-fetch");
 const cheerio = require("cheerio");
 
+const {
+  stripNullValues,
+  fetchResource,
+  parseFeedStream,
+  parseOpmlStream,
+} = require("../lib/common");
+
 module.exports = (init, program) => {
   program
     .command("add-feed [url]")
@@ -83,7 +90,7 @@ async function command(url, options, context) {
     log.error("Failed to import feed: %s", e);
     return exit();
   }
-  
+
   try {
     await feed.pollFeed(context);
   } catch (e) {
@@ -93,67 +100,3 @@ async function command(url, options, context) {
 
   exit();
 }
-
-async function fetchResource({
-  resourceUrl,
-  prevHeaders = {},
-  timeout = 10000,
-  force = false,
-}) {
-  const fetchOptions = {
-    method: "GET",
-    headers: {
-      "user-agent": "glitch-feeder/1.0 (+https://glitch.com/~lmo-feeder)",
-      accept: "application/rss+xml, text/rss+xml, text/xml",
-    },
-  };
-
-  // Set up an abort timeout - we're not waiting forever for a feed
-  const controller = new AbortController();
-  const abortTimeout = setTimeout(() => controller.abort(), parseInt(timeout));
-  fetchOptions.signal = controller.signal;
-
-  // Set up some headers for conditional GET so we can see
-  // some of those sweet 304 Not Modified responses
-  if (!force) {
-    if (prevHeaders.etag) {
-      fetchOptions.headers["If-None-Match"] = prevHeaders.etag;
-    }
-    if (prevHeaders["last-modified"]) {
-      fetchOptions.headers["If-Modified-Match"] = prevHeaders["last-modified"];
-    }
-  }
-
-  try {
-    // Finally, fire off the GET request for the feed resource.
-    const response = await fetch(resourceUrl, fetchOptions);
-    clearTimeout(abortTimeout);
-    return response;
-  } catch (err) {
-    clearTimeout(abortTimeout);
-    throw err;
-  }
-}
-
-const parseFeedStream = ({ stream, resourceUrl }, context) =>
-  new Promise((resolve, reject) => {
-    let meta;
-    const items = [];
-
-    const parser = new FeedParser({
-      addmeta: false,
-      feedurl: resourceUrl,
-    });
-
-    parser.on("error", reject);
-    parser.on("end", () => resolve({ meta, items }));
-    parser.on("readable", function() {
-      meta = this.meta;
-      let item;
-      while ((item = this.read())) {
-        items.push(item);
-      }
-    });
-
-    stream.pipe(parser);
-  });
--- models/Feed.js
+++ models/Feed.js
@@ -7,7 +7,12 @@ const OpmlParser = require("opmlparser");
 const stream = require("stream");
 const AbortController = require("abort-controller");
 const fetch = require("node-fetch");
-const { stripNullValues } = require("../lib/common");
+const {
+  stripNullValues,
+  fetchResource,
+  parseFeedStream,
+  parseOpmlStream,
+} = require("../lib/common");
 
 const { assign } = Object;
 
@@ -419,90 +424,6 @@ class Feed extends guid(BaseModel) {
     }
   }
 }
-
-async function fetchResource({
-  resourceUrl,
-  prevHeaders = {},
-  timeout = 10000,
-  force = false,
-}) {
-  const fetchOptions = {
-    method: "GET",
-    headers: {
-      "user-agent": "glitch-feeder/1.0 (+https://glitch.com/~lmo-feeder)",
-      accept: "application/rss+xml, text/rss+xml, text/xml",
-    },
-  };
-
-  // Set up an abort timeout - we're not waiting forever for a feed
-  const controller = new AbortController();
-  const abortTimeout = setTimeout(() => controller.abort(), parseInt(timeout));
-  fetchOptions.signal = controller.signal;
-
-  // Set up some headers for conditional GET so we can see
-  // some of those sweet 304 Not Modified responses
-  if (!force) {
-    if (prevHeaders.etag) {
-      fetchOptions.headers["If-None-Match"] = prevHeaders.etag;
-    }
-    if (prevHeaders["last-modified"]) {
-      fetchOptions.headers["If-Modified-Match"] = prevHeaders["last-modified"];
-    }
-  }
-
-  try {
-    // Finally, fire off the GET request for the feed resource.
-    const response = await fetch(resourceUrl, fetchOptions);
-    clearTimeout(abortTimeout);
-    return response;
-  } catch (err) {
-    clearTimeout(abortTimeout);
-    throw err;
-  }
-}
-
-const parseOpmlStream = ({ stream }, { log }) =>
-  new Promise((resolve, reject) => {
-    let meta = {};
-    const items = [];
-
-    const parser = new OpmlParser();
-
-    parser.on("error", reject);
-    parser.on("end", () => resolve({ meta, items }));
-    parser.on("readable", function() {
-      meta = this.meta;
-      let outline;
-      while ((outline = this.read())) {
-        items.push(outline);
-      }
-    });
-
-    stream.pipe(parser);
-  });
-
-const parseFeedStream = ({ stream, resourceUrl }, context) =>
-  new Promise((resolve, reject) => {
-    let meta;
-    const items = [];
-
-    const parser = new FeedParser({
-      addmeta: false,
-      feedurl: resourceUrl,
-    });
-
-    parser.on("error", reject);
-    parser.on("end", () => resolve({ meta, items }));
-    parser.on("readable", function() {
-      meta = this.meta;
-      let item;
-      while ((item = this.read())) {
-        items.push(item);
-      }
-    });
-
-    stream.pipe(parser);
-  });
 
 function getParams(str) {
   var params = str.split(";").reduce(function(params, param) {
